595f0453eb5122d262c04bb3d8b977808c738c18
Reyhaneh <reyhaneh.gh.davodi@gmail.com>
2023-08-11 23:36:55 +0330

    create web app

---
+++ function.py
@@ -0,0 +1,15 @@
+FILEPATH="todos.txt"
+
+def get_todos(filepath=FILEPATH):
+   """read the text file and return the list of todos """
+   with open(filepath,"r") as file:
+      todos=file.readlines()
+   return todos
+
+def write_todos(local_todos,filepath=FILEPATH):
+   """write the todo item list in the text file"""
+   with open(filepath,"w") as file:
+      file.writelines(local_todos)
+
+if __name__=="__main__":
+   print("this is a test from function file!")
---
+++ web.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import function
+
+todos=function.get_todos()
+def add_todo():
+    todo=st.session_state["new_todo"]+"\n"
+    todos.append(todo)
+    function.write_todos(todos)
+
+st.title("My Todo App")
+st.subheader("this app is to increase your productivity")
+for index,item in enumerate(todos):
+    checkbox=st.checkbox(item,key=item)
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[item]
+        st.experimental_rerun()
+
+st.text_input(label="",placeholder="add new todo...",
+              on_change=add_todo,key="new_todo")
